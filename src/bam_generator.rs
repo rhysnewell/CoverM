1d74197fb0576a35ea1595d5fe2e72dde6c4c86d
Rhys Newell <rhys.newell@uqconnect.edu.au>
2020-06-09 08:01:09 +1000

    debug messages

--- src/bam_generator.rs
+++ src/bam_generator.rs
@@ -238,6 +238,7 @@ impl NamedBamReader for StreamingNamedBamReader {
     }
 
     fn finish(self) {
+        debug!("Finishing NamedBamReader...");
         complete_processes(
             self.processes,
             self.command_strings,
