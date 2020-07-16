57072f05eaaa758f9bd2cf51e1e9ede221944f4d
Rhys Newell <rhys.newell@hdr.qut.edu.au>
2020-07-17 09:56:28 +1000

    removing sized

--- src/bam_generator.rs
+++ src/bam_generator.rs
@@ -18,7 +18,7 @@ use tempfile;
 
 use rust_htslib::bam::errors::Result as HtslibResult;
 
-pub trait NamedBamReader: Sized {
+pub trait NamedBamReader {
     // Name of the stoit
     fn name(&self) -> &str;
 
@@ -39,7 +39,7 @@ pub trait NamedBamReader: Sized {
     fn num_detected_primary_alignments(&self) -> u64;
 }
 
-pub trait NamedBamReaderGenerator<T>: Sized {
+pub trait NamedBamReaderGenerator<T> {
     // For readers that map, start the process of mapping
     fn start(self) -> T;
 }
